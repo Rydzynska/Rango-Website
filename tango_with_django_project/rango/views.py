fed3feae4a57b93b23da015cf95526c0f103b7d5
Agnieszka <rydzynska.agnieszka@gmail.com>
2016-10-06 17:40:11 +0200

    Added basic index.html and about.html template, and two views to render them; added static and media files

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'aboutmessage': "This website will be nice and running soon!"}
+    return render(request, 'rango/about.html', context=context_dict)
 
