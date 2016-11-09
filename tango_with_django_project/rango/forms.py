e4dc7a19a2b8351b1c649cd82bed2bd31ba200aa
Agnieszka <rydzynska.agnieszka@gmail.com>
2016-11-09 20:41:30 +0100

    Bootstrap completed

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -4,7 +4,7 @@ from rango.models import Page, Category, UserProfile
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128, 
-                            help_text="Please enter the category name.", error_messages={'required': 'Please enter the category.'})
+                            help_text="Please enter the category name", error_messages={'required': 'Please enter the category.'})
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -16,9 +16,9 @@ class CategoryForm(forms.ModelForm):
 
 class PageForm(forms.ModelForm):
     title = forms.CharField(max_length=128,
-                            help_text="Please enter the title of the page.")
+                            help_text="Please enter the title of the page")
     url = forms.URLField(max_length=200,
-                            help_text="Please enter the URL of the page.", initial='http://')
+                            help_text="Please enter the URL of the page", initial='http://')
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
